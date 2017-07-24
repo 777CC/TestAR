f138df434e3f4dea101e213cca28dce5fb66fb6a
Chain Choonoi <chain777@hotmail.com>
2017-07-24 15:27:50 +0700

    change to LP2CPP

---
+++ ios/Classes/Native/Il2CppCompilerCalculateTypeValues_13Table.cpp
@@ -0,0 +1,298 @@
+﻿#include "il2cpp-config.h"
+
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+#include <cstring>
+#include <string.h>
+#include <stdio.h>
+#include <cmath>
+#include <limits>
+#include <assert.h>
+
+#include "class-internals.h"
+#include "codegen/il2cpp-codegen.h"
+#include "UnityEngine_UnityEngine_Events_PersistentListenerMo857969000.h"
+#include "UnityEngine_UnityEngine_Events_ArgumentCache4810721.h"
+#include "UnityEngine_UnityEngine_Events_BaseInvokableCall2229564840.h"
+#include "UnityEngine_UnityEngine_Events_InvokableCall2183506063.h"
+#include "UnityEngine_UnityEngine_Events_UnityEventCallState3420894182.h"
+#include "UnityEngine_UnityEngine_Events_PersistentCall3793436469.h"
+#include "UnityEngine_UnityEngine_Events_PersistentCallGroup339478082.h"
+#include "UnityEngine_UnityEngine_Events_InvokableCallList2295673753.h"
+#include "UnityEngine_UnityEngine_Events_UnityEventBase828812576.h"
+#include "UnityEngine_UnityEngine_Events_UnityAction4025899511.h"
+#include "UnityEngine_UnityEngine_Events_UnityEvent408735097.h"
+#include "UnityEngine_UnityEngine_UnityString276356480.h"
+#include "UnityEngine_UnityEngine_ThreadAndSerializationSafe4226409784.h"
+#include "UnityEngine_UnityEngine_Collections_ReadOnlyAttribu689702060.h"
+#include "UnityEngine_UnityEngine_Collections_ReadWriteAttri3403607913.h"
+#include "UnityEngine_UnityEngine_Collections_WriteOnlyAttribu14323075.h"
+#include "UnityEngine_UnityEngine_Collections_DeallocateOnJob987733588.h"
+#include "UnityEngine_UnityEngine_Collections_NativeContainer269240268.h"
+#include "UnityEngine_UnityEngine_Collections_NativeContaine3267933728.h"
+#include "UnityEngine_UnityEngine_Collections_NativeContaine1288953595.h"
+#include "UnityEngine_UnityEngine_Experimental_Director_Fram1120735295.h"
+#include "UnityEngine_UnityEngine_Experimental_Director_Frame658788566.h"
+#include "UnityEngine_UnityEngine_Internal_DefaultValueAttri1027170048.h"
+#include "UnityEngine_UnityEngine_Internal_ExcludeFromDocsAtt665825653.h"
+#include "UnityEngine_UnityEngine_Logger3328995178.h"
+#include "UnityEngine_UnityEngine_Networking_PlayerConnection301283622.h"
+#include "UnityEngine_UnityEngine_Networking_PlayerConnectio3517219175.h"
+#include "UnityEngine_UnityEngine_Networking_PlayerConnectio2252784345.h"
+#include "UnityEngine_UnityEngine_Networking_PlayerConnectio2167079021.h"
+#include "UnityEngine_UnityEngine_Networking_PlayerConnection536719976.h"
+#include "UnityEngine_UnityEngine_Networking_PlayerConnectio2291506050.h"
+#include "UnityEngine_UnityEngine_Networking_PlayerConnectio1899782350.h"
+#include "UnityEngine_UnityEngine_Experimental_Rendering_Rend984453155.h"
+#include "UnityEngine_UnityEngine_Scripting_UsedByNativeCode3212052468.h"
+#include "UnityEngine_UnityEngine_Scripting_RequiredByNative1913052472.h"
+#include "UnityEngine_UnityEngine_Serialization_FormerlySeri3673080018.h"
+#include "UnityEngine_UnityEngineInternal_TypeInferenceRules1810425448.h"
+#include "UnityEngine_UnityEngineInternal_TypeInferenceRuleA1390152093.h"
+#include "UnityEngine_UnityEngineInternal_NetFxCoreExtension4275971970.h"
+
+
+
+
+
+
+
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1300 = { sizeof (PersistentListenerMode_t857969000)+ sizeof (Il2CppObject), sizeof(int32_t), 0, 0 };
+extern const int32_t g_FieldOffsetTable1300[8] = 
+{
+	PersistentListenerMode_t857969000::get_offset_of_value___1() + static_cast<int32_t>(sizeof(Il2CppObject)),
+	0,
+	0,
+	0,
+	0,
+	0,
+	0,
+	0,
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1301 = { sizeof (ArgumentCache_t4810721), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1301[6] = 
+{
+	ArgumentCache_t4810721::get_offset_of_m_ObjectArgument_0(),
+	ArgumentCache_t4810721::get_offset_of_m_ObjectArgumentAssemblyTypeName_1(),
+	ArgumentCache_t4810721::get_offset_of_m_IntArgument_2(),
+	ArgumentCache_t4810721::get_offset_of_m_FloatArgument_3(),
+	ArgumentCache_t4810721::get_offset_of_m_StringArgument_4(),
+	ArgumentCache_t4810721::get_offset_of_m_BoolArgument_5(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1302 = { sizeof (BaseInvokableCall_t2229564840), -1, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1303 = { sizeof (InvokableCall_t2183506063), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1303[1] = 
+{
+	InvokableCall_t2183506063::get_offset_of_Delegate_0(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1304 = { 0, 0, 0, 0 };
+extern const int32_t g_FieldOffsetTable1304[1] = 
+{
+	0,
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1305 = { 0, 0, 0, 0 };
+extern const int32_t g_FieldOffsetTable1305[1] = 
+{
+	0,
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1306 = { 0, 0, 0, 0 };
+extern const int32_t g_FieldOffsetTable1306[1] = 
+{
+	0,
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1307 = { 0, 0, 0, 0 };
+extern const int32_t g_FieldOffsetTable1307[1] = 
+{
+	0,
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1308 = { 0, 0, 0, 0 };
+extern const int32_t g_FieldOffsetTable1308[1] = 
+{
+	0,
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1309 = { sizeof (UnityEventCallState_t3420894182)+ sizeof (Il2CppObject), sizeof(int32_t), 0, 0 };
+extern const int32_t g_FieldOffsetTable1309[4] = 
+{
+	UnityEventCallState_t3420894182::get_offset_of_value___1() + static_cast<int32_t>(sizeof(Il2CppObject)),
+	0,
+	0,
+	0,
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1310 = { sizeof (PersistentCall_t3793436469), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1310[5] = 
+{
+	PersistentCall_t3793436469::get_offset_of_m_Target_0(),
+	PersistentCall_t3793436469::get_offset_of_m_MethodName_1(),
+	PersistentCall_t3793436469::get_offset_of_m_Mode_2(),
+	PersistentCall_t3793436469::get_offset_of_m_Arguments_3(),
+	PersistentCall_t3793436469::get_offset_of_m_CallState_4(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1311 = { sizeof (PersistentCallGroup_t339478082), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1311[1] = 
+{
+	PersistentCallGroup_t339478082::get_offset_of_m_Calls_0(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1312 = { sizeof (InvokableCallList_t2295673753), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1312[4] = 
+{
+	InvokableCallList_t2295673753::get_offset_of_m_PersistentCalls_0(),
+	InvokableCallList_t2295673753::get_offset_of_m_RuntimeCalls_1(),
+	InvokableCallList_t2295673753::get_offset_of_m_ExecutingCalls_2(),
+	InvokableCallList_t2295673753::get_offset_of_m_NeedsUpdate_3(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1313 = { sizeof (UnityEventBase_t828812576), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1313[4] = 
+{
+	UnityEventBase_t828812576::get_offset_of_m_Calls_0(),
+	UnityEventBase_t828812576::get_offset_of_m_PersistentCalls_1(),
+	UnityEventBase_t828812576::get_offset_of_m_TypeName_2(),
+	UnityEventBase_t828812576::get_offset_of_m_CallsDirty_3(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1314 = { sizeof (UnityAction_t4025899511), sizeof(Il2CppMethodPointer), 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1315 = { sizeof (UnityEvent_t408735097), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1315[1] = 
+{
+	UnityEvent_t408735097::get_offset_of_m_InvokeArray_4(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1316 = { 0, 0, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1317 = { 0, 0, 0, 0 };
+extern const int32_t g_FieldOffsetTable1317[1] = 
+{
+	0,
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1318 = { 0, 0, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1319 = { 0, 0, 0, 0 };
+extern const int32_t g_FieldOffsetTable1319[1] = 
+{
+	0,
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1320 = { 0, 0, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1321 = { 0, 0, 0, 0 };
+extern const int32_t g_FieldOffsetTable1321[1] = 
+{
+	0,
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1322 = { 0, 0, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1323 = { 0, 0, 0, 0 };
+extern const int32_t g_FieldOffsetTable1323[1] = 
+{
+	0,
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1324 = { sizeof (UnityString_t276356480), -1, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1325 = { sizeof (ThreadAndSerializationSafeAttribute_t4226409784), -1, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1326 = { sizeof (ReadOnlyAttribute_t689702060), -1, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1327 = { sizeof (ReadWriteAttribute_t3403607913), -1, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1328 = { sizeof (WriteOnlyAttribute_t14323075), -1, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1329 = { sizeof (DeallocateOnJobCompletionAttribute_t987733588), -1, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1330 = { sizeof (NativeContainerAttribute_t269240268), -1, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1331 = { sizeof (NativeContainerSupportsAtomicWriteAttribute_t3267933728), -1, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1332 = { sizeof (NativeContainerSupportsMinMaxWriteRestrictionAttribute_t1288953595), -1, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1333 = { sizeof (FrameData_t1120735295)+ sizeof (Il2CppObject), sizeof(FrameData_t1120735295 ), 0, 0 };
+extern const int32_t g_FieldOffsetTable1333[6] = 
+{
+	FrameData_t1120735295::get_offset_of_m_FrameID_0() + static_cast<int32_t>(sizeof(Il2CppObject)),
+	FrameData_t1120735295::get_offset_of_m_DeltaTime_1() + static_cast<int32_t>(sizeof(Il2CppObject)),
+	FrameData_t1120735295::get_offset_of_m_Weight_2() + static_cast<int32_t>(sizeof(Il2CppObject)),
+	FrameData_t1120735295::get_offset_of_m_EffectiveWeight_3() + static_cast<int32_t>(sizeof(Il2CppObject)),
+	FrameData_t1120735295::get_offset_of_m_EffectiveSpeed_4() + static_cast<int32_t>(sizeof(Il2CppObject)),
+	FrameData_t1120735295::get_offset_of_m_Flags_5() + static_cast<int32_t>(sizeof(Il2CppObject)),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1334 = { sizeof (Flags_t658788566)+ sizeof (Il2CppObject), sizeof(int32_t), 0, 0 };
+extern const int32_t g_FieldOffsetTable1334[3] = 
+{
+	Flags_t658788566::get_offset_of_value___1() + static_cast<int32_t>(sizeof(Il2CppObject)),
+	0,
+	0,
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1335 = { sizeof (DefaultValueAttribute_t1027170048), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1335[1] = 
+{
+	DefaultValueAttribute_t1027170048::get_offset_of_DefaultValue_0(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1336 = { sizeof (ExcludeFromDocsAttribute_t665825653), -1, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1337 = { 0, -1, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1338 = { 0, -1, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1339 = { sizeof (Logger_t3328995178), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1339[3] = 
+{
+	Logger_t3328995178::get_offset_of_U3ClogHandlerU3Ek__BackingField_0(),
+	Logger_t3328995178::get_offset_of_U3ClogEnabledU3Ek__BackingField_1(),
+	Logger_t3328995178::get_offset_of_U3CfilterLogTypeU3Ek__BackingField_2(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1340 = { sizeof (MessageEventArgs_t301283622), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1340[2] = 
+{
+	MessageEventArgs_t301283622::get_offset_of_playerId_0(),
+	MessageEventArgs_t301283622::get_offset_of_data_1(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1341 = { 0, -1, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1342 = { sizeof (PlayerConnection_t3517219175), -1, sizeof(PlayerConnection_t3517219175_StaticFields), 0 };
+extern const int32_t g_FieldOffsetTable1342[3] = 
+{
+	PlayerConnection_t3517219175::get_offset_of_m_PlayerEditorConnectionEvents_2(),
+	PlayerConnection_t3517219175::get_offset_of_m_connectedPlayers_3(),
+	PlayerConnection_t3517219175_StaticFields::get_offset_of_s_Instance_4(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1343 = { sizeof (PlayerEditorConnectionEvents_t2252784345), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1343[3] = 
+{
+	PlayerEditorConnectionEvents_t2252784345::get_offset_of_messageTypeSubscribers_0(),
+	PlayerEditorConnectionEvents_t2252784345::get_offset_of_connectionEvent_1(),
+	PlayerEditorConnectionEvents_t2252784345::get_offset_of_disconnectionEvent_2(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1344 = { sizeof (MessageEvent_t2167079021), -1, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1345 = { sizeof (ConnectionChangeEvent_t536719976), -1, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1346 = { sizeof (MessageTypeSubscribers_t2291506050), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1346[3] = 
+{
+	MessageTypeSubscribers_t2291506050::get_offset_of_m_messageTypeId_0(),
+	MessageTypeSubscribers_t2291506050::get_offset_of_subscriberCount_1(),
+	MessageTypeSubscribers_t2291506050::get_offset_of_messageCallback_2(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1347 = { sizeof (U3CInvokeMessageIdSubscribersU3Ec__AnonStorey0_t1899782350), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1347[1] = 
+{
+	U3CInvokeMessageIdSubscribersU3Ec__AnonStorey0_t1899782350::get_offset_of_messageId_0(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1348 = { 0, -1, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1349 = { 0, -1, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1350 = { sizeof (RenderPipelineManager_t984453155), -1, sizeof(RenderPipelineManager_t984453155_StaticFields), 0 };
+extern const int32_t g_FieldOffsetTable1350[2] = 
+{
+	RenderPipelineManager_t984453155_StaticFields::get_offset_of_s_CurrentPipelineAsset_0(),
+	RenderPipelineManager_t984453155_StaticFields::get_offset_of_U3CcurrentPipelineU3Ek__BackingField_1(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1351 = { sizeof (UsedByNativeCodeAttribute_t3212052468), -1, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1352 = { sizeof (RequiredByNativeCodeAttribute_t1913052472), -1, 0, 0 };
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1353 = { sizeof (FormerlySerializedAsAttribute_t3673080018), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1353[1] = 
+{
+	FormerlySerializedAsAttribute_t3673080018::get_offset_of_m_oldName_0(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1354 = { sizeof (TypeInferenceRules_t1810425448)+ sizeof (Il2CppObject), sizeof(int32_t), 0, 0 };
+extern const int32_t g_FieldOffsetTable1354[5] = 
+{
+	TypeInferenceRules_t1810425448::get_offset_of_value___1() + static_cast<int32_t>(sizeof(Il2CppObject)),
+	0,
+	0,
+	0,
+	0,
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1355 = { sizeof (TypeInferenceRuleAttribute_t1390152093), -1, 0, 0 };
+extern const int32_t g_FieldOffsetTable1355[1] = 
+{
+	TypeInferenceRuleAttribute_t1390152093::get_offset_of__rule_0(),
+};
+extern const Il2CppTypeDefinitionSizes g_typeDefinitionSize1356 = { sizeof (NetFxCoreExtensions_t4275971970), -1, 0, 0 };
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
